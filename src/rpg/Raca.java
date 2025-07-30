cd3edb0818feda0e0d15594a1dee2f227bd848ac
felício <86389089+lxfelicio@users.noreply.github.com>
2025-07-29 21:40:27 -0300

    Create Raca.java

---
+++ src/rpg/Raca.java
@@ -0,0 +1,12 @@
+package rpg;
+
+/**
+ * ✨ Enum que representa as raças disponíveis no jogo.
+ * Aplica Abstração com conceitos simplificados de herança racial.
+ */
+public enum Raca {
+    HUMANO,
+    GNOMO,
+    ELFO,
+    ORC
+}
